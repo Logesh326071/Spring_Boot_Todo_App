27f7339da7aa70e33c33177b99ddd7a4ed721845
LOGESH PALANISAMY <logu326071@gmail.com>
2026-01-05 16:19:31 +0530

    Delete ToDo-App-Test/ToDo-App/ToDo-App directory

--- ToDo-App-Test/ToDo-App/ToDo-App/src/main/java/com/todo/ToDo_App/Repositary/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todo.ToDo_App.Repositary;
-
-import com.todo.ToDo_App.Entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo,Long> {
-
-}
